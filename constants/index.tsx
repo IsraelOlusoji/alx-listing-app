eb08f99e86399984604a102731cc64f17ffc847b
jungles001 <olusojiisraelife@gmail.com>
2025-09-04 10:58:13 +0100

    alx

--- constants/index.tsx
+++ constants/index.tsx
@@ -2,3 +2,19 @@ export const API_URL = "https://api.example.com";
 export const APP_NAME = "ALX Listing App";
 export const ITEMS_PER_PAGE = 10;
 export const SUPPORTED_LANGUAGES = ["en", "es", "fr"];
+
+
+
+export const UI_TEXT = {
+    "en": {
+        "appName": "ALX Listing App",
+        "itemsPerPage": 10,
+        "supportedLanguages": ["en", "es", "fr"]
+    },
+    "es": {
+        "appName": "ALX Listing App",
+        "itemsPerPage": 10,
+        "supportedLanguages": ["en", "es", "fr"]
+    }
+}
+
