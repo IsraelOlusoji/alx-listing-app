fc29afceec789aa8aef633e361354cea64ccf97d
jungles001 <olusojiisraelife@gmail.com>
2025-09-04 10:23:26 +0100

    alx-listing-app

---
+++ alx-listing-app/constants/index.tsx
@@ -0,0 +1,4 @@
+export const API_URL = "https://api.example.com";
+export const APP_NAME = "ALX Listing App";
+export const ITEMS_PER_PAGE = 10;
+export const SUPPORTED_LANGUAGES = ["en", "es", "fr"];
